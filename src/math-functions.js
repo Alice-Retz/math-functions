e504bf86db885a9d2e70085b49472324189c8b82
Alice-Retz <alicegretz@gmail.com>
2021-07-14 19:16:23 -0500

    function 6 test passing.

--- src/math-functions.js
+++ src/math-functions.js
@@ -139,7 +139,12 @@ This function should be dynamic, accepting an array of any length.
 */
 
 export function multiplyAnyArray(dynamicArray) { //eslint-disable-line
+    let arrVal = 1;
 
+    for(let factor of dynamicArray) {
+        arrVal = multiply(arrVal, factor)[0];
+    }
+    return [arrVal, `The numbers ${dynamicArray} have a product of ${arrVal}.`];
 }
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
